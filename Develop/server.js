6de935fe3dd0c468abcdb5c250e39743555dac03
Kellie Ryan <littlekel03@gmail.com>
2021-05-16 12:04:59 +0800

    added files fir first commmit

---
+++ Develop/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fs = require('fs');
+const app = express()
+const port = 8000
+const path = require('path')
+const id = require('nanoid');
+
+app.use(express.json());
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+
+app.post('/api/notes', (req, res) => {
+    newnote = req.body;
+    req.body.id = id.nanoid(10);
+    console.log(newnote)
+
+    fs.readFile('./db/db.json', (error, data) => {
+        if (error) throw error;
+        let noteArray = JSON.parse(data)
+        noteArray.push(newnote)
+
+        fs.writeFile(
+            "./db/db.json",
+            JSON.stringify(noteArray),
+            function (error) {
+                if (error) throw error;
+                console.log("The Note was saved!");
+            }
+        )
+    })
+})
+
+app.delete('/api/notes:id', (req, res) => {
+    const deleteNote = req.params.id
+
+    fs.readFile('./db/db.json', (error, data) => {
+        if (error) throw error;
+        let noteArray = JSON.parse(data)
+        newNoteArray = noteArray.filter((note) => note.id !== deleteNote)
+
+        fs.writeFile("./db/db.json",
+            JSON.stringify(newNoteArray),
+            function (error) {
+                if (error) throw error;
+                console.log("The Note was deleted!")
+            })
+
+    })
+})
+
+app.listen(port, () => console.log(`I'm listening on port ${port}!`));
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# note-taker
-A very handy note taker and task planner
+# Note Taker
+## Description
+A very handy note taker and task planner to help keep you on track!
+
+## Screenshot
+
+![image]
+
+## Usage
+
+* Github: 
+* Deployed Website:
+
